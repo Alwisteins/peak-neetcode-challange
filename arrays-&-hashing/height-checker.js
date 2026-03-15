22cc1680274a7e663f11af414786b39c6dc39f2d
Alwisteins <alwi.wahyu.w@gmail.com>
2026-03-15 10:58:39 +0700

    challenge 34/954

---
+++ arrays-&-hashing/height-checker.js
@@ -0,0 +1,18 @@
+class Solution {
+  /**
+   * @param {number[]} heights
+   * @return {number}
+   */
+  heightChecker(heights) {
+    const expected = heights.toSorted((a, b) => a - b);
+    let count = 0;
+
+    for (let i = 0; i < heights.length; i++) {
+      if (heights[i] !== expected[i]) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+}
