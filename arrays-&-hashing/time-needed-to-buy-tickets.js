7260cfcf2a8cd0fdbe04d73bb88dd8c804f02e84
Alwisteins <alwi.wahyu.w@gmail.com>
2026-04-08 18:58:05 +0700

    challenge 60

---
+++ arrays-&-hashing/time-needed-to-buy-tickets.js
@@ -0,0 +1,11 @@
+function timeRequiredToBuy(tickets, k) {
+  let time = 0;
+  for (let i = 0; i < tickets.length; i++) {
+    if (i <= k) {
+      time += Math.min(tickets[i], tickets[k]);
+    } else {
+      time += Math.min(tickets[i], tickets[k] - 1);
+    }
+  }
+  return time;
+}
